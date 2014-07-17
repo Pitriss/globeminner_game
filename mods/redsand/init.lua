70f8096928abf894553f625c37ae854b1fc30689
Pitriss <devnull@wjake.com>
2014-07-17 16:36:32 +0200

    redsand: configuration is now read from world dir,

--- mods/redsand/init.lua
+++ mods/redsand/init.lua
@@ -1,9 +1,32 @@
---[[ This mod was made by Teodor Spæren (TheRedMood) ]]--
+--[[ This mod was made by Teodor Spï¿½ren (TheRedMood) ]]--
 
 -- Let the loading begin
+redsand = {}
+redsand.worldpath = minetest.get_worldpath()
+redsand.modpath = minetest.get_modpath("redsand")
 
 -- Essential components
-dofile(minetest.get_modpath("redsand").."/conf.lua")
+dofile(redsand.modpath.."/conf.lua")
+
+if io.open(redsand.worldpath.."/redsand_conf.lua","r") == nil then
+
+	io.input(redsand.modpath.."/conf.lua")
+	io.output(redsand.worldpath.."/redsand_conf.lua")
+
+	local size = 2^13      -- good buffer size (8K)
+	while true do
+		local block = io.read(size)
+		if not block then
+			io.close()
+			break
+		end
+		io.write(block)
+	end
+
+else
+	dofile(redsand.worldpath.."/redsand_conf.lua")
+end
+
 dofile(minetest.get_modpath("redsand").."/functions.lua")
 
 dofile(minetest.get_modpath("redsand").."/messages.lua")
