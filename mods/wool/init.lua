dc5b0c98cb45df06f3b8ff2e608dd37903ba498b
Pitriss <devnull@wjake.com>
2014-07-22 22:00:24 +0200

    Wool: redo to be visible in craft_guide

--- mods/wool/init.lua
+++ mods/wool/init.lua
@@ -10,26 +10,25 @@ local wool = {}
 -- colors available. When crafting, the last recipes will be checked first.
 wool.dyes = {
 	{"white",      "White",      nil},
-	{"grey",       "Grey",       "basecolor_grey"},
-	{"black",      "Black",      "basecolor_black"},
-	{"red",        "Red",        "basecolor_red"},
-	{"yellow",     "Yellow",     "basecolor_yellow"},
-	{"green",      "Green",      "basecolor_green"},
-	{"cyan",       "Cyan",       "basecolor_cyan"},
-	{"blue",       "Blue",       "basecolor_blue"},
-	{"magenta",    "Magenta",    "basecolor_magenta"},
-	{"orange",     "Orange",     "excolor_orange"},
-	{"violet",     "Violet",     "excolor_violet"},
-	{"brown",      "Brown",      "unicolor_dark_orange"},
-	{"pink",       "Pink",       "unicolor_light_red"},
-	{"dark_grey",  "Dark Grey",  "unicolor_darkgrey"},
-	{"dark_green", "Dark Green", "unicolor_dark_green"},
+	{"grey",       "Grey"},
+	{"black",      "Black"},
+	{"red",        "Red"},
+	{"yellow",     "Yellow"},
+	{"green",      "Green"},
+	{"cyan",       "Cyan"},
+	{"blue",       "Blue"},
+	{"magenta",    "Magenta"},
+	{"orange",     "Orange"},
+	{"violet",     "Violet"},
+	{"brown",      "Brown"},
+	{"dark_grey",  "Dark Grey"},
+	{"dark_green", "Dark Green"},
+	{"pink",       "Pink"},
 }
 
 for _, row in ipairs(wool.dyes) do
 	local name = row[1]
 	local desc = row[2]
-	local craft_color_group = row[3]
 	-- Node Definition
 	minetest.register_node("wool:"..name, {
 		description = desc.." Wool",
@@ -37,13 +36,9 @@ for _, row in ipairs(wool.dyes) do
 		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=3,flammable=3,wool=1},
 		sounds = default.node_sound_defaults(),
 	})
-	if craft_color_group then
-		-- Crafting from dye and white wool
-		minetest.register_craft({
-			type = "shapeless",
-			output = 'wool:'..name,
-			recipe = {'group:dye,'..craft_color_group, 'group:wool'},
-		})
-	end
+	minetest.register_craft({
+		type = "shapeless",
+		output = 'wool:'..name,
+		recipe = {'dye:'..name, 'wool:white'},
+	})
 end
-
